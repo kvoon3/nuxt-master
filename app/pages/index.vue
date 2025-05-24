f1d86a87e6028234387fc6294810c56758e0ee1e
kvoon <a1176079417@gmail.com>
2025-05-24 15:30:37 +0800

    feat: nuxt ui

---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <UButton>Test</UButton>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+  ],
 
   devtools: { enabled: true },
+
+  css: [
+    '~/styles/main.css',
+  ],
 
   future: {
     compatibilityVersion: 4,
